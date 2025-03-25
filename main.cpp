9a6954b069b65e668fe56e61f42fcdc327ce0a67
HishamSaeed <hisham.saeed.92@gmail.com>
2025-03-25 13:23:02 +0100

    added git ignore and initiated the program

---
+++ main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>  // Include the I/O stream library
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0;  // Return 0 to indicate successful execution
+}
