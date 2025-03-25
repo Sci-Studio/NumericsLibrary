14964ecb006e6b514ae613a6061d986d329cc630
HishamSaeed <hisham.saeed.92@gmail.com>
2025-03-25 14:49:20 +0100

    removed hello world

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <iostream>  // Include the I/O stream library
 
 int main() {
-    // Print "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
+    
     ObjParser parser("../../Data/rectangle-prism-final.obj");
     parser.parseObjFile();
     parser.printObjFile();
